9cb5caedf981d4ae648890f7da5b03295a5aadba
Tiss29 <tissjoseph2001@gmail.com>
2020-11-09 15:53:27 +0530

    intialize django

--- users/views.py
+++ users/views.py
@@ -6,10 +6,10 @@ def register(request):
  if request.method =='POST':
     form = UserRegisterForm(request.POST)
     if form.is_valid():
-      form.save()
-      username = form.cleaned_data.get('username')
-      messages.success(request,f'Your account has been created!You are able to log in')
-      return redirect('login')
+       form.save()
+       username = form.cleaned_data.get('username')
+       messages.success(request,f'Your account has been created!You are able to log in')
+       return redirect('login')
  else:
      form = UserRegisterForm()
      return render(request,'users/register.html',{'form':  form})
