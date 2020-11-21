ecdeff446abd5bd497c4fe3108a64677cddf413d
Tiss29 <tissjoseph2001@gmail.com>
2020-11-21 20:54:38 +0530

    intialize django

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from .views import PostListView,PostDetailView
 from . import views
 urlpatterns=[
-           path('',views.home,name='blog_home'),
+           path('', PostListView.as_view(), name='blog_home'),
+           path('post/<int:pk>/', PostDetailView.as_view(), name='post_detail'),
            path('about/', views.about,name='blog_about'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,11 +1,21 @@
 from django.shortcuts import render
 from .models import post
-# Create your views here.
+from django.views.generic import ListView,DetailView
 
 def home(request):
  contest={
        'posts':post.objects.all()
    }
  return render(request,'blog/home.html',contest)
+
+class PostListView(ListView):
+    model = post
+    template_name = 'blog/home.html'  # <app>/<model>_<viewtype>.html
+    context_object_name = 'posts'
+    ordering = ['-date']
+
+class PostDetailView(ListView):
+    model = post
+    
 def about(request):
   return render(request,'blog/about.html',{'title':'about'})
